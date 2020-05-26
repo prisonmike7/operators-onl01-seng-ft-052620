e1f8d7b754ede422b39f3dc2d4715596018bd6f2
Michael Harris <mwharris7@gmail.com>
2020-05-26 21:23:03 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,13 @@
 def unsafe?(speed)
-  return speed > 60 || speed < 40 ? true : false
+  if speed > 60 || speed < 40
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-
+  return speed > 60 || speed < 40 ? true : false
 end
